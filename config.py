b5cf2d8fc26af0dfada65e15ce1345a6a28f91bb
EasyIP2023 <vdavis2495@gmail.com>
2019-02-11 09:28:23 -0600

    remove my dev db

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'deveopment.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
