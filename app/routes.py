66df6dea14edcdcbaf06958e80271d1300f02143
Utkarsh Singh <udg538@my.utsa.edu>
2019-03-27 22:52:48 -0700

    Proto2 working.. FAQ and Landing page needed

--- app/routes.py
+++ app/routes.py
@@ -45,8 +45,6 @@ def upload():
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -61,24 +59,6 @@ def index():
         return redirect(next_page)
     return render_template('index.html', title='Home', form=form)
     #return render_template('index.html', title='Home')
-
-#@app.route('/login', methods=['GET', 'POST'])
-#def login():
-#    if current_user.is_authenticated:
-#        return redirect(url_for('index'))
-#    form = LoginForm()
-#    if form.validate_on_submit():
-#        user = User.query.filter_by(username=form.username.data).first()
-#        if user is None or not user.check_password(form.password.data):
-#            flash(u'Invalid username or password','error')
-#            return redirect(url_for('login'))
-#        login_user(user, remember=form.remember_me.data)
-#        next_page = request.args.get('next')
-#        if not next_page or url_parse(next_page).netloc != '':
-#            next_page = url_for('index')
-#        flash(u'Successfully Signed in!!!', 'success')
-#        return redirect(next_page)
-#    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -96,6 +76,7 @@ def register():
 def logout():
     logout_user()
     flash(u'Successfully Signed out', 'success')
+#    return render_template('index.html', title='Home', form=form) 
     return redirect(url_for('index'))
 
 # Admin interface class's
