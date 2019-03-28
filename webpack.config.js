b823dae6f65772bbb14d56c20612149789d838b1
EasyIP2023 <vdavis2495@gmail.com>
2019-03-28 17:56:29 +0000

    add js & css folders

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const webpack = require('webpack');
 const config = {
   // This is 100% not the best way to do this!!!
   entry: {
-    app_js:  __dirname + '/app/assets/application.js',
-    app_css:  __dirname + '/app/assets/application.css',
+    app_js:  __dirname + '/app/assets/js/application.js',
+    app_css:  __dirname + '/app/assets/css/application.css',
     bootstrap_css: __dirname + '/app/vendor/css/bootstrap.min.css',
     bootstrap_js: __dirname + '/app/vendor/js/bootstrap.min.js',
   },
