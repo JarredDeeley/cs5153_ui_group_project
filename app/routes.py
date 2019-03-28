d11ff3203e943071beccc47b9e30eefe0a94cdbe
Utkarsh Singh <udg538@my.utsa.edu>
2019-03-27 21:18:45 -0700

    Prototype 2

--- app/routes.py
+++ app/routes.py
@@ -43,12 +43,8 @@ def upload():
 
 # Where actual routes start
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='Home')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
@@ -56,14 +52,33 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash(u'Invalid username or password','error')
-            return redirect(url_for('login'))
+            return redirect(url_for('index'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         flash(u'Successfully Signed in!!!', 'success')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('index.html', title='Home', form=form)
+    #return render_template('index.html', title='Home')
+
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+#    if current_user.is_authenticated:
+#        return redirect(url_for('index'))
+#    form = LoginForm()
+#    if form.validate_on_submit():
+#        user = User.query.filter_by(username=form.username.data).first()
+#        if user is None or not user.check_password(form.password.data):
+#            flash(u'Invalid username or password','error')
+#            return redirect(url_for('login'))
+#        login_user(user, remember=form.remember_me.data)
+#        next_page = request.args.get('next')
+#        if not next_page or url_parse(next_page).netloc != '':
+#            next_page = url_for('index')
+#        flash(u'Successfully Signed in!!!', 'success')
+#        return redirect(next_page)
+#    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -73,7 +88,7 @@ def register():
     if form.validate_on_submit():
         form.save()
         flash(u'Congratulations, you are now a registered user!', 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/logout")
@@ -127,6 +142,7 @@ class AdminTopicView(FlaskView):
         return render_template('admin/topics/index.html', title='Topics',
                                 topics=Topic.query.all())
 
+
     def post(self, msg):
         form = TopicForm()
         if form.validate_on_submit():
@@ -174,3 +190,5 @@ class AdminUserView(FlaskView):
 
     def show(self, id):
         return render_template('admin/users/show.html', user=User.query.get(id))
+
+
